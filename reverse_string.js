d8ce101b78654ff3ba50176fbd2d5b22b12925cb
Salma Elshaelshahawy <salma71@gmail.com>
2018-07-01 18:31:53 -0400

    reverse a string

---
+++ reverse_string.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    let reversed = '';
+    for (let char of str) {
+        reversed = char + reversed
+    }
+    return reversed
+}
+const str1 = reverse('apple')
+str1
+
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
