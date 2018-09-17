297daa55f642278328f4dd5778e7779389466b6b
Salma Elshahawy <salmaeng71@gmail.com>
2018-09-17 14:17:19 -0400

    add fibonacci solution using yield keyword

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,26 @@
 // print out the n-th entry in the fibonacci series
-//[0,1,1,2,3,5,8,13]
+// [0,1,1,2,3,5,8,13]
+// [0,1,Fn-1, Fn-1 + Fn-2, ......]
 // fib(4) === 3
-
-function fib(n) {
-    if (n < 2) {
-        return n
-    } else {
-        return fib(n - 1) + fib(n - 2)
-    }
+// Fn = Fn-1 + Fn-2 -> mathematic equation when n>2
+function fib (n) {
+  if (n < 2) {
+    return n
+  } else {
+    return fib(n - 1) + fib(n - 2)
+  }
 }
 fib(4) // -> 3
+
+// using yield key word
+function * fib (n, current = 0, next = 1) {
+  if (n === 0) {
+    return current
+  }
+  yield current
+  yield * fib(n - 1, next, current + next)
+}
+
+let a = [...fib(4)]
+a
+// [0, 1, 1, 2]
